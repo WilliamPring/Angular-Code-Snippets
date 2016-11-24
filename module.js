b77ccd78c7d160dfbb39fc367681a27df56fbedd
William Pring <wp@williampring.com>
2016-11-24 11:01:56 -0500

    Add files via upload
    Putting modules for direct usages of routes

---
+++ module.js
@@ -0,0 +1,8 @@
+//The module name has to match with the one you put in your route
+//the Controller has to be the same name that your provide in the .when( templateUrl: 'Template name',
+//controller: 'Controller name'
+
+//Import addition feature put it in the function parmeters
+angular.module('Module Name', []).controller('Controller Name', function ($http) {
+    //Inside here for the angular code
+});
